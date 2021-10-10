80e0a54e9675de6b40b22593ad9b7bdeb166fd33
Nikhil Joshua <nikhiljoshua.a@gmail.com>
2021-10-10 18:06:47 +0530

    Add ApiClient interface and CL Client

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -6,21 +6,17 @@ class HttpClient {
     private $url;
     private $ch;
     private $res;
-    public function __construct($url = 'http://api.countrylayer.com/v2/all?access_key=615a7f2c90da86de92cfb0a2415e726c'){
-        $this->url = $url;
+    private $apiClient;
+    public function __construct(ApiClient $apiClient){
+        $this->url = $apiClient->getUrl();
+        $this->apiClient = $apiClient;
         $this->ch = curl_init();
         curl_setopt($this->ch, CURLOPT_URL,$this->url);
         curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
     }
 
     public function fetch(){
-        $data = file_get_contents(__DIR__ . '/cache.txt');
-        if(!$data) {
-            $this->res = curl_exec($this->ch);
-            file_put_contents(__DIR__ . '/cache.txt', $this->res);
-            return;
-        }
-        $this->res = $data;
+        $this->res = $this->apiClient->fetch($this->ch);
     }
 
     public function display(){
@@ -30,5 +26,37 @@ class HttpClient {
     public function __destruct()
     {
         curl_close($this->ch);
+    }
+}
+
+interface ApiClient {
+    public function fetch($ch);
+    public function getUrl();
+}
+
+class CountryLayerAPI implements ApiClient {
+    private $url;
+    private $res;
+    public function __construct($accessToken)
+    {
+        $this->url = 'http://api.countrylayer.com/v2/all?access_key=' . $accessToken;
+    }
+
+    public function fetch($ch)
+    {
+        $data = file_get_contents(__DIR__ . '/CLcache.txt');
+        if(!$data) {
+            $this->res = curl_exec($ch);
+            $arr = json_decode($this->res, true);
+            $slice = array_slice($arr, 0,15);
+            $this->res = json_encode($slice);
+            file_put_contents(__DIR__ . '/CLcache.txt', $this->res);
+            return $this->res;
+        }
+        return $data;
+    }
+
+    public function getUrl(){
+        return $this->url;
     }
 }
--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
 require_once __DIR__ . '/HttpClient.php';
-$httpClient = new HttpClient();
+$httpClient = new HttpClient(new CountryLayerAPI('615a7f2c90da86de92cfb0a2415e726c'));
 $httpClient->fetch();
 $httpClient->display();
